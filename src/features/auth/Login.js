b3f8bd25112d9390cd2cfff1ab3c87c69ae4470d
Hamed Jimoh <hameed.jimoh1601@gmail.com>
2023-02-02 14:20:51 +0100

    Implements login component JSX and functionalities

--- src/features/auth/Login.js
+++ src/features/auth/Login.js
@@ -1,7 +1,98 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setCredentials } from "./authSlice";
+import { useLoginMutation } from "./authApiSlice";
 
 const Login = () => {
-  return <h1>Login</h1>;
+  const userRef = useRef();
+  const errRef = useRef();
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errMsg, setErrMsg] = useState("");
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [login, { isLoading }] = useLoginMutation();
+
+  useEffect(() => {
+    setErrMsg("");
+  }, [username, password]);
+
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
+
+  const handleUserInput = (e) => setUsername(e.target.value);
+  const handlePwdInput = (e) => setPassword(e.target.value);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const { accessToken } = await login({ username, password }).unwrap();
+      dispatch(setCredentials({ accessToken }));
+      setUsername("");
+      setPassword("");
+    } catch (error) {
+      if (!error.status) {
+        setErrMsg("No response from server");
+      } else if (error.status === 400) {
+        setErrMsg("Invalid username or password");
+      } else if (error.status === 401) {
+        setErrMsg("Unauthorized");
+      } else {
+        setErrMsg(error.data?.message);
+      }
+
+      errRef.current.focus();
+    }
+  };
+
+  const errClass = errMsg ? "errmsg" : "offscreen";
+
+  if (isLoading) return <p>Loading...</p>;
+
+  const content = (
+    <section className="public">
+      <header>
+        <h1>Employee Login</h1>
+      </header>
+      <main className="login">
+        <p ref={errRef} className={errClass} aria-live="assertive">
+          {errMsg}
+        </p>
+        <form className="form" onSubmit={handleSubmit}>
+          <input
+            className="form__input"
+            type="text"
+            id="username"
+            ref={userRef}
+            value={username}
+            onChange={handleUserInput}
+            autoComplete="off"
+            required
+          />
+          <label htmlFor="password">Password:</label>
+          <input
+            className="form__input"
+            type="password"
+            id="password"
+            onChange={handlePwdInput}
+            value={password}
+            required
+          />
+        </form>
+      </main>
+      <footer>
+        <Link to="/">Back to Home</Link>
+      </footer>
+    </section>
+  );
+
+  return content;
 };
 
 export default Login;
