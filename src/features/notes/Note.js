695b162c1d90aeba1d01bf7952a852ea8b40bcfb
Hamed Jimoh <hameed.jimoh1601@gmail.com>
2023-02-05 12:48:46 +0100

    Refactors the Note component

--- src/features/notes/Note.js
+++ src/features/notes/Note.js
@@ -1,12 +1,16 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
+import { memo } from "react";
 
-import { useSelector } from "react-redux";
-import { selectNoteById } from "./notesApiSlice";
+import { useGetNotesQuery } from "./notesApiSlice";
 
 const Note = ({ noteId }) => {
-  const note = useSelector((state) => selectNoteById(state, noteId));
+  const { note } = useGetNotesQuery("NotesList", {
+    selectFromResult: ({ data }) => ({
+      note: data?.entities[noteId],
+    }),
+  });
 
   const navigate = useNavigate();
 
@@ -46,4 +50,6 @@ const Note = ({ noteId }) => {
     );
   } else return null;
 };
-export default Note;
+
+const memoizedNote = memo(Note);
+export default memoizedNote;
