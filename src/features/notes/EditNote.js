b77315dcbd9c52a9f707746cb0b3f5f2e31d569e
Hamed Jimoh <hameed.jimoh1601@gmail.com>
2023-02-05 14:47:08 +0100

    Refactors the EditNote component

--- src/features/notes/EditNote.js
+++ src/features/notes/EditNote.js
@@ -3,19 +3,32 @@ import { useSelector } from "react-redux";
 import { selectNoteById } from "./notesApiSlice";
 import { selectAllUsers } from "../users/usersApiSlice";
 import EditNoteForm from "./EditNoteForm";
+import { useGetNotesQuery } from "./notesApiSlice";
+import { useGetUsersQuery } from "../users/usersApiSlice";
+import useAuth from "../../hooks/useAuth";
+import PulseLoader from "react-spinners/PulseLoader";
 
 const EditNote = () => {
   const { id } = useParams();
+  const { isAdmin, isManager, username } = useAuth();
 
-  const note = useSelector((state) => selectNoteById(state, id));
-  const users = useSelector(selectAllUsers);
+  const { note } = useGetNotesQuery("notesList", ({ data }) => ({
+    note: data?.entities[id],
+  }));
 
-  const content =
-    note && users ? (
-      <EditNoteForm note={note} users={users} />
-    ) : (
-      <p>Loading...</p>
-    );
+  const { users } = useGetUsersQuery("usersList", ({ data }) => ({
+    users: data?.ids.map((id) => data?.entities[id]),
+  }));
+
+  if (!note || !users?.length) return <PulseLoader color="#fff" />;
+
+  if (!isAdmin || !isManager) {
+    if (note.username !== username) {
+      return <p className="errmsg">No access</p>;
+    }
+  }
+
+  const content = <EditNoteForm note={note} users={users} />;
 
   return content;
 };
